526c30af4ca9a2b02960ac110c673b5e521103b0
wahb-amir <op422010@gmail.com>
2026-01-03 00:49:05 +0500

    build fix ts error

--- app/utils/token.ts
+++ app/utils/token.ts
@@ -110,8 +110,10 @@ export const generateToken: GenerateTokenFn = (
         expiresIn = "1h";
     }
   }
+const signOptions: jwt.SignOptions = {
+  expiresIn: typeof expiresIn === "string" ? (expiresIn as unknown as string) : expiresIn,
+};
 
-  const signOptions: jwt.SignOptions = { expiresIn };
 
   return jwt.sign(payload as jwt.JwtPayload, secret, signOptions);
 };
